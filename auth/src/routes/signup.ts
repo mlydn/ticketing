738a66fce11af8c5d220db78cadd51b5f6f6099d
mlydn <59616364+mlydn@users.noreply.github.com>
2024-02-27 10:36:43 +0100

    adding express validation for signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,33 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
- const router = express.Router()
+const router = express.Router()
 
- router.post('/api/users/signup', (req, res) => {
-   res.send('Fit like?')
- })
+router.post('/api/users/signup', [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters')
+  ],
 
- export { router as signupRouter }
+  (req: Request, res: Response) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array())
+    }
+
+    const { email, password } = req.body
+
+    console.log('Creating a user')
+
+    // new User({ email, password})
+
+    res.send({})
+})
+
+export { router as signupRouter }
