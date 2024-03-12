a2c48a718f4dced41e71cd045a982cbe5ace8648
mlydn <59616364+mlydn@users.noreply.github.com>
2024-03-12 11:09:13 +0100

    Move currentuser handling to middleware

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -5,16 +5,7 @@ import { currentUser } from '../middlewares/current-user'
 const router = express.Router()
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null })
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-    res.send({ currentUser: payload })
-  } catch (error) {
-    res.send({ currentUser: null })
-  }
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
